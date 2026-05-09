b33af4fc44f9bb873d0ce3fbf1b35861b87a3238
malyshenko-developer <nikitamalyshenkodeveloper@gmail.com>
2026-05-09 18:27:38 +0800

    chore: add patch-package, clean app.json

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "@/global.css"
 
 export default function App() {
 	return (
-		<View className="flex-1 items-center justify-center bg-white bg-[#1E1B2E]">
+		<View className="flex-1 items-center justify-center bg-[#1E1B2E]">
 			<Text className="text-xl font-bold text-blue-300">
 				Welcome to Cozy Pomodoro!
 			</Text>
